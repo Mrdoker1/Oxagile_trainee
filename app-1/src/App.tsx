3b6c84725f6ef137ed821091c84143859a1b66dd
Viachaslau Kulbitski <fixrapdok@gmail.com>
2024-03-28 21:15:19 +0300

    init: vite buider

--- app-1/src/App.tsx
+++ app-1/src/App.tsx
@@ -1,11 +1,9 @@
-import React from 'react';
-
 function App() {
   return (
-    <div className="App">
-      Hello world!
+    <div>
+      Hello World!
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
